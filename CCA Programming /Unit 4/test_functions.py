01951cc97be6fc73d7092cfe21ba3c62a55b1b26
AnnamarieMcIntosh <40639844+AnnamarieMcIntosh@users.noreply.github.com>
2018-10-17 18:30:40 -0700

    Add files via upload

---
+++ CCA Programming /Unit 4/test_functions.py
@@ -0,0 +1,15 @@
+def increment (x):
+    """
+    Special comment:
+    This is called a docstring.
+    This is where you describe what your function does,
+    including what are the expected inputs.
+    """
+    x = x + 1
+    return x
+
+
+def no_return():
+    """ This function does absolutely nothing."""
+    print("This function doesn't return anything.")
+
--- CCA Programming /Unit 5/draw.py
+++ CCA Programming /Unit 5/draw.py
@@ -1,8 +1,9 @@
-# Annamarie McIntosh
-# Drawing With Turtles Part 1
+## Annamarie McIntosh
+## Drawing With Turtles Part 1
 
 import turtle
 
+# makes coral spiral
 turtle.pencolor("coral1")
 turtle.penup()
 turtle.goto(200,200)
@@ -11,6 +12,16 @@ for i in range(39):
     turtle.forward(200)
     turtle.left(123)
 turtle.penup()
+
+# make coral star
+turtle.goto(305, 220)
+turtle.pendown()
+for i in range(5):
+    turtle.forward(75)
+    turtle.right(144)
+turtle.penup()
+
+# makes blue spiral 
 turtle.goto(-200, 200)
 turtle.pencolor("deepskyblue2")
 turtle.pendown()
@@ -18,17 +29,55 @@ for i in range(39):
     turtle.forward(200)
     turtle.left(123)
 turtle.penup()
-turtle.goto(-200, -100)
+
+# make blue star
+turtle.goto(-263,285)
+turtle.pendown()
+for i in range(5):
+    turtle.forward(75)
+    turtle.right(144)
+turtle.penup()
+
+# makes pink spiral
+turtle.goto(-250, -100)
 turtle.pencolor("orchid2")
 turtle.pendown()
 for i in range(39):
     turtle.forward(200)
     turtle.left(123)
-    turtle.penup()
+turtle.penup()
+
+# make pink star
+turtle.goto(-299,-200)
+turtle.pendown()
+for i in range(5):
+    turtle.forward(75)
+    turtle.right(144)
+turtle.penup()
+
+# makes green spiral
 turtle.goto(200, -250)
 turtle.pencolor("chartreuse2")
 turtle.pendown()
 for i in range(39):
     turtle.forward(200)
     turtle.left(123)
-    
+turtle.penup()
+
+# make green star
+turtle.goto(305,-245)
+turtle.pendown()
+for i in range(5):
+    turtle.forward(75)
+    turtle.right(144)
+turtle.penup()
+
+# makes words in the center
+turtle.home()
+turtle.pencolor("black")
+turtle.pendown()
+turtle.write("hello ;)", move=False, align="center", font=("Comic Sans MS", 100, "normal"))
+turtle.penup()
+
+
+
